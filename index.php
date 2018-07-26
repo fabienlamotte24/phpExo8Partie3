9a2820d0cd2a37b343a9a16b4aaa7311ccc63891
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 15:18:59 +0200

    exo8 partie 3 php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+<?php
+  for ($pas = 200; $pas >= 0; $pas -= 12){
+    echo $pas . ' pas, enfin !!!<br />';
+  }
+ ?>
+    </p>
+  </body>
+</html>
