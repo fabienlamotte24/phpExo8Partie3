fa619d4174559daad0798dde9df5a2c880f180df
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 08:44:09 +0200

    exo8p3 php

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
   <body>
     <p>
 <?php
+/*Utilisation de la boucle for
+1 - Variable qui débute à 200
+2 - Tant que $pas est suppérieur à 0
+3 - Décrémentation par 12 (on retire 12 à la valeur)*/
   for ($pas = 200; $pas >= 0; $pas -= 12){
+    /*Affichage*/
     echo $pas . ' pas, enfin !!!<br />';
   }
  ?>
